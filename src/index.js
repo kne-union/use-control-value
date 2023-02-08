099ed2a01ac4a247751120234c999efe038fae9d
zhipenglin <sunandmoon001@163.com>
2023-02-08 19:10:00 +0800

    修改当onChange的参数是function时的调用错误行为

--- src/index.js
+++ src/index.js
@@ -7,15 +7,11 @@
 import React, {useState, useRef} from 'react';
 
 export default (props, map = {defaultValue: 'defaultValue', value: 'value', onChange: 'onChange'}) => {
-    const isControl = useRef(map['value'] in props),
-        [stateValue, setStateValue] = useState(props[map['defaultValue']]);
-    return [
-        isControl.current ? props[map['value']] : stateValue,
-        (newValue, ...others) => {
-            props[map['onChange']] && props[map['onChange']](newValue, ...others);
-            if (!isControl.current) {
-                setStateValue(newValue);
-            }
+    const isControl = useRef(map['value'] in props), [stateValue, setStateValue] = useState(props[map['defaultValue']]);
+    return [isControl.current ? props[map['value']] : stateValue, (newValue, ...others) => {
+        props[map['onChange']] && props[map['onChange']](typeof newValue === 'function' ? newValue(isControl.current ? props[map['value']] : stateValue) : newValue, ...others);
+        if (!isControl.current) {
+            setStateValue(newValue);
         }
-    ];
+    }];
 };
