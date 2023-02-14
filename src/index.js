e0220df48363a64449c2190ba8f2dabbe2dd282b
zhipenglin <sunandmoon001@163.com>
2023-02-14 14:57:43 +0800

    修改额外参数丢失问题

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-/**
- * @name: useControlValue ;
- * @author: admin ;
- * @description: 实现了同时支持受控和非受控组件状态管理的hooks ;
- * */
+import React, {useState, useRef} from "react";
 
-import React, {useState, useRef} from 'react';
-
-export default (props, map = {defaultValue: 'defaultValue', value: 'value', onChange: 'onChange'}) => {
-    const isControl = useRef(map['value'] in props), [stateValue, setStateValue] = useState(props[map['defaultValue']]);
-    return [isControl.current ? props[map['value']] : stateValue, (newValue, ...others) => {
-        props[map['onChange']] && props[map['onChange']](typeof newValue === 'function' ? newValue(isControl.current ? props[map['value']] : stateValue) : newValue, ...others);
+export default (props, map = {defaultValue: "defaultValue", value: "value", onChange: "onChange"}) => {
+    const isControl = useRef(map["value"] in props), [stateValue, setStateValue] = useState(props[map["defaultValue"]]);
+    const controlValueRef = useRef(null);
+    controlValueRef.current = props[map["value"]];
+    return [isControl.current ? props[map["value"]] : stateValue, (newValue, ...others) => {
+        const newState = typeof newValue === "function" ? controlValueRef.current = newValue(controlValueRef.current) : newValue;
+        props[map["onChange"]] && props[map["onChange"]](newState, ...others);
         if (!isControl.current) {
-            setStateValue(newValue);
+            setStateValue(newState);
         }
     }];
 };
