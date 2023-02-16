79cbe6cfdedda1f82c29e2acc36510915e6d0794
zhipenglin <sunandmoon001@163.com>
2023-02-16 15:05:15 +0800

    修正非受控模式默认值问题

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React, {useState, useRef} from "react";
 
 export default (props, map = {defaultValue: "defaultValue", value: "value", onChange: "onChange"}) => {
     const isControl = useRef(map["value"] in props), [stateValue, setStateValue] = useState(props[map["defaultValue"]]);
-    const controlValueRef = useRef(null);
-    controlValueRef.current = props[map["value"]];
-    return [isControl.current ? props[map["value"]] : stateValue, (newValue, ...others) => {
-        const newState = typeof newValue === "function" ? controlValueRef.current = newValue(controlValueRef.current) : newValue;
+    const currentValueRef = useRef(null);
+    currentValueRef.current = isControl.current ? props[map["value"]] : stateValue;
+    return [currentValueRef.current, (newValue, ...others) => {
+        const newState = typeof newValue === "function" ? currentValueRef.current = newValue(currentValueRef.current) : newValue;
         props[map["onChange"]] && props[map["onChange"]](newState, ...others);
         if (!isControl.current) {
             setStateValue(newState);
